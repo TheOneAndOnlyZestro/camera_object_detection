a5748b3b8813ec5a4125b23c4d637c65f7aa3396
GIU F1tenth Jetson <fam@awadlouis.com>
2025-03-13 22:14:59 +0100

    Created the basic file structure for camera object detection submodule

---
+++ src/camera_obj_detection/camera_obj_detection/camera_subscriber.py
@@ -0,0 +1,23 @@
+from rclpy.node import Node
+import rclpy
+class CameraObjDetection(Node):
+    def __init__(self):
+        super().__init__('camera_obj_detection')
+        self.test = self.create_timer(0.5, self.test_callback)
+
+    def test_callback(self):
+        self.get_logger().info("This node is working!!!!")
+
+
+#Apply in main function
+
+def main(args=None):
+    rclpy.init(args=args)
+    cod = CameraObjDetection()
+
+    rclpy.spin(cod)
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
