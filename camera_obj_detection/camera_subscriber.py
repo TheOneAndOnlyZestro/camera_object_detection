83ab055c6e3dbcaf102696fef74e5046c917d410
TheOneAndOnlyZestro <theoneandonlyzestro@gmail.com>
2025-03-14 19:10:02 +0100

    Created a simple node which subscribes to the camera raw_image topic and shows it using opencv library

--- camera_obj_detection/camera_subscriber.py
+++ camera_obj_detection/camera_subscriber.py
@@ -1,12 +1,23 @@
-from rclpy.node import Node
 import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
 class CameraObjDetection(Node):
     def __init__(self):
         super().__init__('camera_obj_detection')
-        self.test = self.create_timer(0.5, self.test_callback)
+        self.subscriber_ = self.create_subscription(
+        Image,
+        "/camera/camera/color/image_raw",
+        self.test_callback,
+        10)
 
-    def test_callback(self):
-        self.get_logger().info("This node is working, again!!!!!!!!!!!")
+    def test_callback(self, msg):
+        cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+
+            # You can display the image or just print some basic information
+        cv2.imshow("RealSense Camera Image", cv_image)
+        cv2.waitKey(1)  # Refresh window every 1 ms
 
 
 #Apply in main function
