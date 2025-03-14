7f03568a49496ab5ed424f166d4b43e0787c9aa2
TheOneAndOnlyZestro <theoneandonlyzestro@gmail.com>
2025-03-14 20:36:20 +0100

    Added the model file and setup publishing on /camera_obj_prediction for publishing annotated images

--- camera_obj_detection/camera_subscriber.py
+++ camera_obj_detection/camera_subscriber.py
@@ -3,6 +3,7 @@ from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
+from ultralytics import YOLO
 class CameraObjDetection(Node):
     def __init__(self):
         super().__init__('camera_obj_detection')
@@ -12,12 +13,21 @@ class CameraObjDetection(Node):
         self.test_callback,
         10)
         self.bridge = CvBridge()
+        self.publisher_ = self.create_publisher(Image, "/camera_obj_prediction",10)
+        self.model = YOLO("best.pt")
+        self.model.fuse()
     def test_callback(self, msg):
         cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+        
+        results = self.model(cv_image)
+        
+        annotated_image = results[0].plot()  
+        output_msg = self.bridge.cv2_to_imgmsg(annotated_image, encoding="bgr8")
 
-            # You can display the image or just print some basic information
-        cv2.imshow("RealSense Camera Image", cv_image)
-        cv2.waitKey(1)  # Refresh window every 1 ms
+        self.publisher_.publish(output_msg)
+        
+        cv2.imshow("YOLOv8 Object Detection", annotated_image)
+        cv2.waitKey(1)
 
 
 #Apply in main function
